86a9e53c6ffa6b26a21763d583e1977b31a96207
Ramon A. Garcia <ramon2garcia8@gmail.com>
2023-06-26 11:57:16 -0400

    sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(userName, userEmail) {
+    const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify({
+          name: userName,
+          email: userEmail,
+        }),
+      };
+      
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+        return response.json();
+      })
+      .then(function (object) {
+        document.body.append(object.id);
+        //console.log(object.id);
+      })
+      .catch(function (error) {
+        document.body.append(error.message);
+        console.log(error.message);
+      });
+}
